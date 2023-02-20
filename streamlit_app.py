7fa4f2efb8e6cd9910944c530759f2ef78328d53
Yogesh Rai <63515909+yograi343@users.noreply.github.com>
2023-02-20 21:48:21 +0530

    updated emojis with the text
    added emojis with the text in the

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smotthie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('🥣 Breakfast Menu')
+streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🐔 Kale, Spinach & Rocket Smotthie')
+streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')

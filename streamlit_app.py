938b490891309fb3464b3f5e55d700b1334a8251
Yogesh Rai <63515909+yograi343@users.noreply.github.com>
2023-02-23 15:28:37 +0530

    indentation was not proper

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,11 +28,11 @@ try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
       streamlit.error("Please select a fruit to get information.")
-   else:
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-      fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
-      streamlit.dataframe(fruityvice_normalized)
-      streamlit.write('The user entered',fruit_choice)
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+    streamlit.write('The user entered',fruit_choice)
 except URLEroor as e:
   streamlit.error()
 

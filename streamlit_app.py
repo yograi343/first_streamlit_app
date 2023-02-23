5eac29990ab49dd8cb8b82946c19f87ea7a54790
Yogesh Rai <63515909+yograi343@users.noreply.github.com>
2023-02-23 13:03:18 +0530

    json version of the response and normalize it
    json_normalize function used from the pandas python library ad streamlit.dataframe is used to showcase the data in table format.

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,3 +26,7 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
+#take the json version of the response and normalize it
+fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
+#output it the screen as a table
+streamlit.dataframe(fruityvice_normalized)

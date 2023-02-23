f09654185634437de44a78798bbd4419ff9ef72e
Yogesh Rai <63515909+yograi343@users.noreply.github.com>
2023-02-23 12:58:58 +0530

    Header and Json format added for api
    Header for the section added and json format is provided in the request to tell the app to showcase the response in json format.

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,10 @@ fruits_to_show=my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
+#New section to display fruityvice api response
+stramlit.header('Fruityvice Fruit Advice!')
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
 

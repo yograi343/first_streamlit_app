6396b8f45cfa2cf915742843c52c3c00f2964534
Yogesh Rai <63515909+yograi343@users.noreply.github.com>
2023-02-23 16:07:34 +0530

    my cur was instead of my_cur

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ except URLEroor as e:
 streamlit.header("The fruit load list contains:")
 #snowflake-related functions
 def get_fruit_load_list():
-  with my_cnx.cursor() as my cur:
+  with my_cnx.cursor() as my_cur:
     my_cur.execute("select * from fruit_load_list")
     return my_cur.fetchall()
 #add a button to load the fruit

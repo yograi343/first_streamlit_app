79adb9c94609f91f720ba0d429d4ede4276d0259
Yogesh Rai <63515909+yograi343@users.noreply.github.com>
2023-02-23 13:09:34 +0530

    giving user to choose the fruit name
    using text_input function from streamlit and setting default value as Kiwi . Created a variable which will get concatenated based on the user value whose default value is Kiwi.

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,12 +21,15 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+Streamlit.write("The user entered',fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
 
 #take the json version of the response and normalize it
 fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
 #output it the screen as a table
 streamlit.dataframe(fruityvice_normalized)
+

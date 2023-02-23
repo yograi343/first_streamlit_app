e19c03734cd50a30cc6b3149492bf24b78e180df
Yogesh Rai <63515909+yograi343@users.noreply.github.com>
2023-02-23 15:20:20 +0530

    troubleshooting the code.
    put all the import at the top
    streamlit stop is executed to not run

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit
+import snowflake.connector
+import pandas
+import requests
 
 streamlit.title("My Mom's New Healthy Diner")
 
@@ -9,7 +12,7 @@ streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
+
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -24,16 +27,16 @@ streamlit.header('Fruityvice Fruit Advice!')
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered',fruit_choice)
 
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-
 
 #take the json version of the response and normalize it
 fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
 #output it the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
+#don't run anything past here while we trobleshoot
+streamlit.stop()
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
@@ -43,3 +46,6 @@ streamlit.dataframe(my_data_row)
 
 add_my_fruit = streamlit.text_input('What fruit you like to add?')
 streamlit.write('Thanks for adding',add_my_fruit)
+
+#This will not work corrrectly but just go with with it for now
+my_cur.excecute("insert into fruit_load_list values ('from streamlit')")

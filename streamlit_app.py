6234a55d67b63afc56983db5f7f0041745776b8b
Yogesh Rai <63515909+yograi343@users.noreply.github.com>
2023-02-23 13:10:24 +0530

    inverted commas was not correctly entered.

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ streamlit.dataframe(fruits_to_show)
 #New section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-Streamlit.write("The user entered',fruit_choice)
+Streamlit.write('The user entered',fruit_choice)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
